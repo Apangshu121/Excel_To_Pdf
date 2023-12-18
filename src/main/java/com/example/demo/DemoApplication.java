536c779ec43bdb04396d055c72113e49a1d266f7
Apangshu121 <apangshu.debnath.accolitedigital.com>
2023-12-18 23:46:39 +0530

    Fourth Commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -27,20 +27,19 @@ import java.util.List;
 
 @SpringBootApplication
 public class DemoApplication {
-    static Document document;
+	static Document document;
 	static PdfWriter writer;
 
-	private static void createAPdf()
-	{
-		try{
+	private static void createAPdf() {
+		try {
 			//PDF document with the A4 page size is created
 			document = new Document(PageSize.A4);
 			String filename = "queries.pdf";
 			// PdfWriter instance for writing content to the PDF file.
-			writer = PdfWriter.getInstance(document,new FileOutputStream(filename));
-		}catch (DocumentException e) {
+			writer = PdfWriter.getInstance(document, new FileOutputStream(filename));
+		} catch (DocumentException e) {
 			e.printStackTrace();
-		}catch (FileNotFoundException e) {
+		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
 	}
@@ -83,37 +82,39 @@ public class DemoApplication {
 		// An array of JFreeChart objects is created.
 		// Each chart is created by calling the createChart method with specific parameters.
 		JFreeChart[] charts = new JFreeChart[]{
-				createChart("Top 3 panels for the month of Oct and Nov", panels.get(0).getPanelName(),panels.get(0).getNoOfInterviews(), panels.get(1).getPanelName(), panels.get(1).getNoOfInterviews(), panels.get(2).getPanelName(), panels.get(2).getNoOfInterviews()),
-				createChart("Top 3 skills for the months of Oct and Nov", skills.get(0), Integer.parseInt(freq.get(0)),skills.get(1), Integer.parseInt(freq.get(1)), skills.get(2), Integer.parseInt(freq.get(2))),
-				createChart("Top 3 skills for which interviews were conducted", skills1.get(0), Integer.parseInt(freq1.get(0)),skills1.get(1), Integer.parseInt(freq1.get(1)), skills1.get(2), Integer.parseInt(freq1.get(2)))
+				createChart("Top 3 panels for the month of Oct and Nov", panels.get(0).getPanelName(), panels.get(0).getNoOfInterviews(), panels.get(1).getPanelName(), panels.get(1).getNoOfInterviews(), panels.get(2).getPanelName(), panels.get(2).getNoOfInterviews()),
+				createChart("Top 3 skills for the months of Oct and Nov", skills.get(0), Integer.parseInt(freq.get(0)), skills.get(1), Integer.parseInt(freq.get(1)), skills.get(2), Integer.parseInt(freq.get(2))),
+				createChart("Top 3 skills for which interviews were conducted", skills1.get(0), Integer.parseInt(freq1.get(0)), skills1.get(1), Integer.parseInt(freq1.get(1)), skills1.get(2), Integer.parseInt(freq1.get(2)))
 		};
 
 		//The code calculates the height available for each chart on the PDF page by dividing the total height of an A4 page by the number of charts.
-		float chartHeight = PageSize.A4.getHeight()/ charts.length;
+		float chartHeight = PageSize.A4.getHeight() / charts.length;
 
 		//The code iterates through each chart in the array.
-		for(int i=0;i<charts.length;i++)
-		{
+		for (int i = 0; i < charts.length; i++) {
+			//A PdfContentByte object is created from the PdfWriter object. This object allows you to write content to the PDF document.
 			PdfContentByte contentByte = writer.getDirectContent();
-			// For each chart, it creates a Pdf template with the specified size.
-			PdfTemplate template = contentByte.createTemplate(PageSize.A4.getWidth(),chartHeight);
-			// It creates a Graphics2D object from the template and draws the current chart onto the template.
+			//A PdfTemplate object is created using the createTemplate method on the contentByte object. The size of the template is set to the width of an A4 page and a specified chart height.
+			PdfTemplate template = contentByte.createTemplate(PageSize.A4.getWidth(), chartHeight);
+			// It creates a Graphics2D object from the template and helps to draw shapes, text, and images onto the template.
 			Graphics2D graphics2D = template.createGraphics(PageSize.A4.getWidth(), chartHeight);
 
-			// Draw the charts on the template
-			charts[i].draw(graphics2D,new Rectangle2D.Double(0,0,PageSize.A4.getWidth(), chartHeight));
+			//The draw method is called on the current chart object, which draws the chart onto the Graphics2D object. The chart is drawn within a rectangle that has the same width as an A4 page and the specified chart height.
 
-			//Close the graphics object
+			charts[i].draw(graphics2D, new Rectangle2D.Double(0, 0, PageSize.A4.getWidth() / 2, chartHeight));
+
+			//The Graphics2D object is disposed of to free up system resources.
 			graphics2D.dispose();
 
-			// Add the template to the document at the correct position
-			contentByte.addTemplate(template,0,PageSize.A4.getHeight() - chartHeight*(i+1));
+			// The addTemplate method is called on the contentByte object to add the template (which now contains the drawn chart) to the PDF document.
+			contentByte.addTemplate(template, 0, PageSize.A4.getHeight() - chartHeight * (i + 1));
 		}
 		document.close();
 
 	}
+
 	private static JFreeChart createChart(String title, String type1, int value1, String type2, int value2, String type3, int value3) {
-		// It creates a DefaultPieDataset, which is a dataset for pie charts.
+		// It creates a DefaultPieDataset object, this object is used to store the data that will be displayed on the pie chart.
 		DefaultPieDataset dataset = new DefaultPieDataset();
 		// Adds the data to the pieChart.
 		dataset.setValue(type1, value1);
@@ -128,7 +129,7 @@ public class DemoApplication {
 				true,
 				false);
 
-		// Retrieves the piePlot from the pieChart
+		// Retrieves the plot from the pieChart which is cast to PiePlot.
 		PiePlot plot = (PiePlot) pieChart.getPlot();
 		// We are setting the labels {0} represents type {1} represents the values and {2} repesents the percentage
 		plot.setLabelGenerator(new StandardPieSectionLabelGenerator("{0} = {1} ({2})"));
